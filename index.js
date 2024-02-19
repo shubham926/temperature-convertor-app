78d497341f71226a671692522fd7941715394e91
shubham926 <shubham412302@gmail.com>
2024-02-19 15:35:27 +0530

    intital upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const textBox = document.getElementById("textBox");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelsius = document.getElementById("toCelsius");
+const result = document.getElementById("result");
+let temp;
+
+function convert() {
+  console.log("convert function called");
+  if (toFahrenheit.checked) {
+    temp = Number(textBox.value);
+    temp = (temp * 9) / 5 + 32;
+    result.textContent = temp.toFixed(1) + "°F";
+  } else if (toCelsius.checked) {
+    temp = Number(textBox.value);
+    temp = (temp - 32) * (5 / 9);
+    result.textContent = temp.toFixed(1) + "°C";
+  } else {
+    result.textContent = "Please select a unit";
+  }
+}
